84e64ad23162c69b30ce28024457ba42ac2e6f2c
niyazidageek <niyazinb@code.edu.az>
2021-08-01 12:56:57 +0400

    footer done, active navbar remaining

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -4,25 +4,44 @@ import { Button } from './SectionCard';
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <div className="container footer-container d-flex justify-content-between">
-                <div className="row flex-column">
-                <h3>Contacts</h3>
-                <h2>sasas</h2>
-                </div>
-                <div className="row flex-column">
-                    <div className="row">
-                    <header className="d-flex">
-                    <ul className="d-flex">
+        <div className="footer" id="contacts">
+            <div className="container footer-container d-flex flex-column">
+                <header>
+                    <h2>Contacts</h2>
+                    <nav className="d-flex align-items-center">
+                    <ul>
                         <li>AMSTERDAM</li>
                         <li>BAKU</li>
                         <li>SINGAPORE</li>
-                    </ul>                    
+                    </ul>
                     <Button content="Our branches" />
-                    </header>
+                    </nav>
                     
+                </header>
+               <footer className="d-flex justify-content-between flex-column">
+               <div className="top d-flex justify-content-end w-100">
+                <div className="right">
+                    <article>
+                    <div className="box">
+                        <span>Headquarters</span>
+                        <p>Teleportboulevard 130, Scalehub, Unit number 1.08, 1043 EJ Amsterdam, Netherlands</p>
                     </div>
+                    <div className="box">
+                        <span>E-mail</span>
+                        <p>amsterdam@simbrella.com</p>
+                    </div>
+                    </article>
                 </div>
+               </div>
+               <div className="bottom d-flex justify-content-between">
+                   <div className="left">
+                       <span>Site by Niyazi Babayev</span>
+                   </div>
+                   <div className="right">
+                       <span>Copyright © 2021. All rights reserved.</span>
+                   </div>
+               </div>
+               </footer>
             </div>
         </div>
     );
